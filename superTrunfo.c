0d9c5be5234c0d5cfe842ec0cbad51c866d80622
Lorran <lorranpereiraslv@gmail.com>
2025-04-04 20:06:34 -0300

    Atualização do código com entrada de strings e melhorias

--- superTrunfo.c
+++ superTrunfo.c
@@ -2,10 +2,22 @@
 
 int main(){
 
+    char estado[3], codigo_da_carta[10], nome_da_cidade[50];
     int populacao, numero_de_pontos_turisticos;
     float area, PIB;
 
     //Cadastro da Primeira carta
+
+    printf("=== Cadastro da Primeira Carta ===\n");
+
+    printf("Digite o estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &codigo_da_carta);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nome_da_cidade);
 
     printf("Digite a populacao: \n");
     scanf("%d", &populacao);
@@ -20,6 +32,17 @@ int main(){
     scanf("%f", &PIB);
 
     //Cadastro da Segunda carta
+
+    printf("=== Cadastro da Segunda Carta ===\n");
+
+    printf("Digite o estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo da carta: \n");
+    scanf("%s", &codigo_da_carta);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nome_da_cidade);
 
     printf("Digite a sua populacao: \n");
     scanf("%d", &populacao);
