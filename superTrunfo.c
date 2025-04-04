39b26fc2ddf5c93fd6ba32b6d5c02f0aa396684e
Lorran <lorranpereiraslv@gmail.com>
2025-04-04 19:26:33 -0300

    Primeiro commit

---
+++ superTrunfo.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+
+    int populacao, numero_de_pontos_turisticos;
+    float area, PIB;
+
+    //Cadastro da Primeira carta
+
+    printf("Digite a populacao: \n");
+    scanf("%d", &populacao);
+
+    printf("Numero de pontos turisticos: \n");
+    scanf("%d", &numero_de_pontos_turisticos);
+
+    printf("Digite a sua area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o seu PIB: \n");
+    scanf("%f", &PIB);
+
+    //Cadastro da Segunda carta
+
+    printf("Digite a sua populacao: \n");
+    scanf("%d", &populacao);
+
+    printf("Numero de pontos turisticos: \n");
+    scanf("%d", &numero_de_pontos_turisticos);
+
+    printf("Digite a sua area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o seu PIB: \n");
+    scanf("%f", &PIB);
+
+    return 0;
+
+}
