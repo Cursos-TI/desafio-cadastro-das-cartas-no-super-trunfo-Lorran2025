cfb8aa94f22057eb41612561deb1d58999729d8a
Lorran <lorranpereiraslv@gmail.com>
2025-04-17 01:38:40 -0300

    Atualizando Arquivo

--- desafioSuperTrunfo.c
+++ desafioSuperTrunfo.c
@@ -2,30 +2,26 @@
 
 int main(){
     //Dados da primeira carta
-    char estado1[3], codigo_da_carta1[10], nome_da_cidade1[50];
+    char pais1[10];
     int numero_de_pontos_turisticos1;
     float area1, PIB1, densidade_populacional1, PIB_per_capita1;
     float divisao;
     unsigned long populacao1 = 1000000000;
 
     //Dados da segunda carta
-    char estado2[3], codigo_da_carta2[10], nome_da_cidade2[50];
+    char pais2[10];
     int numero_de_pontos_turisticos2;
     float area2, PIB2, densidade_populacional2, PIB_per_capita2;
     unsigned long populacao2 = 1000000000;
+
+    int opcao;
 
     //Cadastro da Primeira carta
 
     printf("=== Cadastro da Primeira Carta ===\n");
 
-    printf("Digite o estado: \n");
-    scanf("%s", estado1);
-
-    printf("Digite o codigo da carta: \n");
-    scanf("%s", &codigo_da_carta1);
-
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", &nome_da_cidade1);
+    printf("Digite o nome do pais: \n");
+    scanf("%s", &pais1);
 
     printf("Digite a populacao: \n");
     scanf("%lu", &populacao1);
@@ -49,16 +45,10 @@ int main(){
 
     printf("=== Cadastro da Segunda Carta ===\n");
 
-    printf("Digite o estado: \n");
-    scanf("%s", &estado2);
+    printf("Digite o nome do pais: \n");
+    scanf("%s", &pais2);
 
-    printf("Digite o codigo da carta: \n");
-    scanf("%s", &codigo_da_carta2);
-
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", &nome_da_cidade2);
-
-    printf("Digite a sua populacao : \n");
+    printf("Digite a sua populacao: \n");
     scanf("%lu", &populacao2);
 
     printf("Numero de pontos turisticos: \n");
@@ -76,56 +66,95 @@ int main(){
     printf("Densidade Populacional: %.2f hab/km\n", densidade_populacional2);
     printf("Pib per Capita: R$ %.2f\n", PIB_per_capita2);
 
+    printf("### Menu de Comparacao ### \n");
+    printf("1. Populacao \n");
+    printf("2. Numero de Pontos Turisticos \n");
+    printf("3. Area \n");
+    printf("4. PIB \n");
+    printf("5. Densidade Populacional \n");
+    printf("6. PIB per capita \n");
+    printf("Selecione uma opcao: \n");
+    scanf("%d", &opcao);
+
     "=== Comparação das Cartas === \n";
 
+    switch (opcao) {
+    case 1: printf("Atributo: Populacao \n");
+            printf("%s: %lu\n", pais1, populacao1);
+            printf("%s: %lu\n", pais2, populacao2);
     if (populacao1 > populacao2)
-    {    printf("Atributo: Populacao \n");
-         printf("Carta 1 venceu \n");
-    } else
-    {   printf("Atributo: Populacao \n");
-        printf("Carta 2 venceu \n");
-    }
+    {       printf("Carta 1 venceu \n");
+    } else if(populacao2 > populacao1) {
+            printf("Carta 2 venceu \n");
+    }  else
+    {   printf("Empate!! \n");
+    } 
+        break;
     
+    case 2: printf("Atributo: Numero de Pontos Turisticos \n");
+            printf("%s: %d\n", pais1, numero_de_pontos_turisticos1);
+            printf("%s: %d\n", pais2, numero_de_pontos_turisticos2);
     if (numero_de_pontos_turisticos1 > numero_de_pontos_turisticos2)
-    {   printf("Atributo: Numero de pontos turisticos \n");
-        printf("Carta 1 venceu \n");
-    } else
-    { printf("Atributo: Numero de pontos turisticos \n");
-        printf("Carta 2 venceu \n");
-     
-    }
-    
+    {       printf("Carta 1 venceu \n");
+    } else if(numero_de_pontos_turisticos2 > numero_de_pontos_turisticos1) {
+            printf("Carta 2 venceu \n");
+    }  else
+    {   printf("Empate!! \n");
+    } 
+        break;
+
+    case 3: printf("Atributo: Area \n");
+            printf("%s: %.2f\n", pais1, area1);
+            printf("%s: %.2f\n", pais2, area2);
     if (area1 > area2)
-    {   printf("Atributo: Area \n");
-        printf("Carta 1 venceu \n");
-    } else
-        printf("Atributo: Area \n");
-    {   printf("Carta 2 venceu \n");
-    }
+    {       printf("Carta 1 venceu \n");
+    } else if(area2 > area1) {
+            printf("Carta 2 venceu \n");
+    }  else
+    {   printf("Empate!! \n");
+    } 
+        break;
 
+    case 4: printf("Atributo: PIB \n");
+            printf("%s: %.2f\n", pais1, PIB1);
+            printf("%s: %.2f\n", pais2, PIB2);
     if (PIB1 > PIB2)
-    {   printf("Atributo: PIB \n");
-        printf("Carta 1 venceu \n");
-    } else
-    {   printf("Atributo: PIB \n");
-        printf("Carta 2 venceu \n");
-    }
-    
-    if (densidade_populacional1 < densidade_populacional2)
-    {   printf("Atributo: Densidade populacional \n");
-        printf("Carta 1 venceu \n");
-    } else
-    {   printf("Atributo: Densidade populacional \n");
-        printf("Carta 2 venceu \n");
-    }
+    {       printf("Carta 1 venceu \n");
+    } else if(PIB2 > PIB1) {
+            printf("Carta 2 venceu \n");
+    }  else
+    {   printf("Empate!! \n"); 
+    } 
+        break;
 
-    if (PIB_per_capita1 > PIB_per_capita2)
-    {   printf("Atributo: PIB per capita \n");
-        printf("Carta 1 venceu \n");
-    } else
-    {   printf("Atributo: PIB per capita \n");
-        printf("Carta 2 venceu \n");
+    case 5: printf("Atributo: Densidade Populacional \n");
+            printf("%s: %.2f\n", pais1, densidade_populacional1);
+            printf("%s: %.2f\n", pais2, densidade_populacional2);
+    if (densidade_populacional2 > densidade_populacional1)
+    {       printf("Carta 1 venceu \n");
+    } else if(densidade_populacional1 > densidade_populacional2) {
+            printf("Carta 2 venceu \n");
+    }  else
+    {   printf("Empate!! \n");
     }
+        break;
+
+    case 6: printf("PIB per capita \n");
+            printf("%s: %.2f\n", pais1, PIB_per_capita1);
+            printf("%s: %.2f\n", pais2, PIB_per_capita2);
+    if (PIB_per_capita1 > PIB_per_capita2)
+    {       printf("Carta 1 venceu \n");
+    } else if(PIB_per_capita2 > PIB_per_capita1) {
+            printf("Carta 2 venceu \n");
+    }  else
+    {   printf("Empate!! \n");
+    } 
+        break;
+
+    default: 
+        printf("Opcao Invalida \n");
+        break;
     
-    return 0;
+}
+
 }
